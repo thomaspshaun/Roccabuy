f9faf2e0652994024f39b73ace3d6c826fe11c3e
thomaspshaun <shaun.padachi@gmail.com>
2018-05-20 13:02:54 +0200

    Installed woocomerce and storefront (DB - roccabuy(20180520_1301))

---
+++ wp-content/plugins/woocommerce/woocommerce.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Plugin Name: WooCommerce
+ * Plugin URI: https://woocommerce.com/
+ * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
+ * Version: 3.3.5
+ * Author: Automattic
+ * Author URI: https://woocommerce.com
+ *
+ * Text Domain: woocommerce
+ * Domain Path: /i18n/languages/
+ *
+ * @package WooCommerce
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Define WC_PLUGIN_FILE.
+if ( ! defined( 'WC_PLUGIN_FILE' ) ) {
+	define( 'WC_PLUGIN_FILE', __FILE__ );
+}
+
+// Include the main WooCommerce class.
+if ( ! class_exists( 'WooCommerce' ) ) {
+	include_once dirname( __FILE__ ) . '/includes/class-woocommerce.php';
+}
+
+/**
+ * Main instance of WooCommerce.
+ *
+ * Returns the main instance of WC to prevent the need to use globals.
+ *
+ * @since  2.1
+ * @return WooCommerce
+ */
+function wc() {
+	return WooCommerce::instance();
+}
+
+// Global for backwards compatibility.
+$GLOBALS['woocommerce'] = wc();
+
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+
+function custom_override_checkout_fields( $fields ) {
+unset($fields['billing']['billing_company']);
+unset($fields['shipping']['shipping_company']);
+return $fields;
+}
